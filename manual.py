eced22f1e0f0a7876b8b6d629017ce6581367c18
toejough <toejough@gmail.com>
2015-05-02 10:42:56 -0600

    reduced number of files - to run tests manually, run the test file manually.  automated pexpect tests do this (sort of recursively calling the file).  All the test code is defined in the same place then.  Maybe I'll reverse this later, but for now, it's simpler

--- manual.py
+++
@@ -1,5 +0,0 @@
-import pimento
-
-menu = pimento.Menu("yes or no?", ['yes', 'no'], "Please choose: ")
-result = menu.prompt()
-print 'Result was {}'.format(result)
--- test_pimento.py
+++ test_pimento.py
@@ -1,11 +1,28 @@
-import pimento
-import pexpect
+'''
+Test suite for pimento
+'''
 
+
+# [ Imports ]
+# [ -Third Party ]
+import pexpect
+# [ -Project ]
+import pimento
+
+
+# [ Tests ]
 def test_simple_valid():
-    p = pexpect.spawn('python manual.py')
+    p = pexpect.spawn('python test_pimento.py')
     p.expect_exact('yes or no?')
     p.expect_exact('  yes')
     p.expect_exact('  no')
     p.expect_exact('Please choose: ')
     p.sendline('yes')
     p.expect('Result was yes')
+
+
+# [ Manual Interaction ]
+if __name__ == '__main__':
+    menu = pimento.Menu("yes or no?", ['yes', 'no'], "Please choose: ")
+    result = menu.prompt()
+    print 'Result was {}'.format(result)
