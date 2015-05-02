e67aacf12292ad9d7f65641531fe1b58147acd50
toejough <toejough@gmail.com>
2015-05-02 11:03:15 -0600

    simplifying test with a helper function

--- test_pimento.py
+++ test_pimento.py
@@ -10,22 +10,25 @@ import pexpect
 import pimento
 
 
+# [ Helpers ]
+# [ -Setup ]
+def get_menu_process():
+    p = pexpect.spawn('python test_pimento.py', timeout=1)
+    p.expect_exact('yes or no?')
+    p.expect_exact('  yes')
+    p.expect_exact('  no')
+    p.expect_exact('Please choose: ')
+    return p
+
+
 # [ Tests ]
 def test_menu_accepts_full_response():
     # yes
-    p = pexpect.spawn('python test_pimento.py')
-    p.expect_exact('yes or no?')
-    p.expect_exact('  yes')
-    p.expect_exact('  no')
-    p.expect_exact('Please choose: ')
+    p = get_menu_process()
     p.sendline('yes')
     p.expect('Result was yes')
     # no
-    p = pexpect.spawn('python test_pimento.py')
-    p.expect_exact('yes or no?')
-    p.expect_exact('  yes')
-    p.expect_exact('  no')
-    p.expect_exact('Please choose: ')
+    p = get_menu_process()
     p.sendline('no')
     p.expect('Result was no')
 
