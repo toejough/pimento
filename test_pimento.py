95634b102207d50bb21ac60d4db0dc8d0a319554
toejough <toejough@gmail.com>
2015-05-02 10:38:32 -0600

    reduced to just pexpect test - want to simplify menu implentation

--- test_pimento.py
+++ test_pimento.py
@@ -1,27 +1,7 @@
 import pimento
 import pexpect
 
-def test_basic():
-    menu = pimento.Menu("yes or no?", ['yes', 'no'], "Please choose: ")
-    out = menu._out()
-    assert out == '''yes or no?
-  yes
-  no
-Please choose: '''
-    menu._choose('y')
-    assert menu._result == 'yes'
-    menu._choose('ye')
-    assert menu._result == 'yes'
-    menu._choose('yes')
-    assert menu._result == 'yes'
-    menu._choose('n')
-    assert menu._result == 'no'
-    menu._choose('no')
-    assert menu._result == 'no'
-    menu._choose('foo')
-    assert menu._result is pimento.INVALID
-
-def test_basic_interactive():
+def test_simple_valid():
     p = pexpect.spawn('python manual.py')
     p.expect_exact('yes or no?')
     p.expect_exact('  yes')
