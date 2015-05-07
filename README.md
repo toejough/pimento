e106a7068818cf2ba8d7e839826a88b7a92913c9
toejough <toejough@gmail.com>
2015-05-06 23:45:33 -0600

    Released 0.1.0 to pypi

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pimento
 simple CLI menu
+
+# installation
+```bash
+pip install pimento
+```
+
+# testing
+pimento has only been tested on python 2.7.9.
---
+++ setup.py
@@ -0,0 +1,35 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pimento',
+    version='0.1.0',
+    description='Simple CLI Menus',
+    long_description='Python Menus.  Make an interactive menu with a default, pre- and post-prompts, partial argument matching, indexing, etc.',
+    url='https://github.com/toejough/pimento',
+    author='toejough',
+    author_email='toejough@gmail.com',
+    license='MIT',
+    classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+            'Development Status :: 3 - Alpha',
+
+            # Indicate who your project is intended for
+            'Intended Audience :: Developers',
+
+            # Pick your license as you wish (should match "license" above)
+             'License :: OSI Approved :: MIT License',
+
+            # Specify the Python versions you support here. In particular, ensure
+            # that you indicate whether you support Python 2, Python 3 or both.
+            'Programming Language :: Python :: 2.7',
+    ],
+    keywords="menu cli",
+    packages = find_packages(),
+    install_requires = []
+)
