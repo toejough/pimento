1c0284d2a9134f945dee1c0d083e40180c3143e3
toejough <toejough@gmail.com>
2015-05-09 11:18:27 -0600

    Moved arg checks to their own functions.
    Fixes #8

--- pimento/__init__.py
+++ pimento/__init__.py
@@ -82,17 +82,22 @@ def _check_response(response, items, default, indexed):
     return selection
 
 
-# [ Public API ]
-def menu(pre_prompt, items, post_prompt=NO_ARG, default_index=None, indexed=False):
-    '''Prompt with a menu'''
-    # Fill in post-prompt dynamically if no arg
-    if post_prompt is NO_ARG:
-        if default_index is None:
-            post_prompt = "Enter an option to continue: "
-        else:
-            post_prompt = "Enter an option to continue [{}]: "
-    # Check that the list is iterable and finite
+def _check_prompts(pre_prompt, post_prompt):
+    '''Check that the prompts are strings'''
+    if not isinstance(pre_prompt, basestring):
+        raise TypeError("The pre_prompt was not a string!")
+    if post_prompt is not NO_ARG and not isinstance(post_prompt, basestring):
+        raise TypeError("The post_prompt was given and was not a string!")
+
+
+def _check_items(items):
+    '''
+    Check:
+     - that the list of items is iterable and finite.
+     - that the list is not empty
+    '''
     num_items = 0
+    # Check that the list is iterable and finite
     try:
         num_items = len(items)
     except:
@@ -100,34 +105,53 @@ def menu(pre_prompt, items, post_prompt=NO_ARG, default_index=None, indexed=Fals
     # Check that the list has items
     if num_items == 0:
         raise ValueError("The item list is empty.")
-    # Check that the default is in the list:
-    default = None
+
+
+def _check_default_index(items, default_index):
+    '''Check that the default is in the list'''
+    num_items = len(items)
     if default_index is not None and not isinstance(default_index, int):
         raise TypeError("The default index ({}) is not an integer".format(default_index))
-    if default_index is not None and len(items) <= default_index:
-        raise ValueError("The default index ({}) >= length of the list".format(default_index))
+    if default_index is not None and default_index >= num_items:
+        raise ValueError("The default index ({}) >= length of the list ({})".format(default_index, num_items))
     if default_index is not None and default_index < 0:
         raise ValueError("The default index ({}) < 0.".format(default_index))
-    elif default_index is not None:
-        default = items[default_index]
-    # Check that the prompts are strings
-    if not isinstance(pre_prompt, basestring):
-        raise TypeError("The pre_prompt was not a string!")
-    if not isinstance(post_prompt, basestring):
-        raise TypeError("The post_prompt was not a string!")
-    # convert items to strings
-    items = [str(i) for i in items]
-    # TODO check default/prompt here, too (prompt should have a {} in it)
-    # State
+
+
+# [ Public API ]
+def menu(pre_prompt, items, post_prompt=NO_ARG, default_index=None, indexed=False):
+    '''
+    Prompt with a menu.
+    '''
+    # arg checking
+    _check_prompts(pre_prompt, post_prompt)
+    _check_items(items)
+    _check_default_index(items, default_index)
+    # arg mapping
+    # - Fill in post-prompt dynamically if no arg
+    actual_post_prompt = post_prompt
+    if post_prompt is NO_ARG:
+        if default_index is None:
+            actual_post_prompt = "Enter an option to continue: "
+        else:
+            actual_post_prompt = "Enter an option to continue [{}]: "
+    # - convert items to strings
+    actual_items = [str(i) for i in items]
+    # - set the default argument
+    default = None
+    if default_index is not None:
+        default = actual_items[default_index]
+    # other state init
     acceptable_response_given = False
-    selection = None
     # Prompt Loop
     # - wait until an acceptable response has been given
     while not acceptable_response_given:
+        selection = None
         # Prompt and get response
-        response = _prompt(pre_prompt, items, post_prompt, default, indexed)
+        response = _prompt(pre_prompt, actual_items, actual_post_prompt, default, indexed)
         # validate response
-        selection = _check_response(response, items, default, indexed)
+        selection = _check_response(response, actual_items, default, indexed)
+        # NOTE: acceptable response logic is purposely verbose to be clear about the semantics.
         if selection is not None:
             acceptable_response_given = True
     return selection
